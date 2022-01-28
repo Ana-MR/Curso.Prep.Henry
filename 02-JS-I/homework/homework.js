91156061f7a5fbf2c36935c66401a8c84aae7051
Ana-MR <AnaOlgaMontenegro@gmail.com>
2022-01-28 00:11:28 -0300

    igualdad

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,8 @@ const nuevoModulo = 21 % 5 === 1 ;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+
+  return str ;
   
 }
 
@@ -35,24 +37,41 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+
+  var resultado = x + y ;
+
+  return resultado ;
   
 }
+
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+
+  var resultado = x - y ;
+
+  return resultado ;
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+
+  var resultado = x * y ;
+
+  return resultado ;
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+
+  var resultado = x / y ;
+
+  return resultado ;
   
 }
 
@@ -60,7 +79,20 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+
+  var igualdad = false ;
+
+
+  if (x === y) { 
+
+    igualdad = true ;
+  }
+
+
+  return igualdad ;
+
+// return ( x === y )
+
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -206,7 +238,6 @@ function esVocal(letra){
   //Escribe tu código aquí
   
 }
-
 
 
 // No modificar nada debajo de esta línea
